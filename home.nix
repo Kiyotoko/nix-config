b87d3557ef25aa111a872f3fbe044d39412151b7
Kiyotoko <karl.zschiebsch@gmail.com>
2025-03-16 13:47:55 +0100

    Add home.nix

---
+++ home.nix
@@ -0,0 +1,110 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "karl";
+  home.homeDirectory = "/home/karl";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+  home.file.".config/hypr/hyprland.conf" = {
+    source = ./hypr/hyprland.conf;
+    recursive = true;
+  }
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # Utility
+    thunderbird
+    audacious
+    gimp
+
+    # Programming
+    vscodium
+    jetbrains.idea-ultimate
+    jetbrains.rust-rover
+    obsidian
+
+    # Messaging
+    signal-desktop
+    telegram-desktop
+    zapzap
+    discord
+    teamspeak5_client
+
+    # Games
+    lutris
+    inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.faf-client
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+
+    shellAliases = {
+      rb = "sudo nixos-rebuild switch --flake .";
+    };
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "24.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
