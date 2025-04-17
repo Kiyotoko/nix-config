27381e646452a0245f45b36bbf9b892dce136ea2
Kiyotoko <karl.zschiebsch@gmail.com>
2025-04-17 22:57:20 +0200

    Add kio packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -70,7 +70,14 @@
     # -----------------------------------------------------
     # Audio & Video
     # -----------------------------------------------------
-    kdePackages.k3b # burn audio cd
+    # Full-featured CD/DVD/Blu-ray burning and ripping application
+    kdePackages.k3b
+    # Network transparent access to files and data
+    kdePackages.kio
+    # KIO worker to access Google Drive
+    kdePackages.kio-gdrive
+    # Additional components to increase the functionality of KIO
+    kdePackages.kio-extras
     # Highly portable CD/DVD/BluRay command line recording software
     cdrtools
     # Cross-platform media player and streaming server
