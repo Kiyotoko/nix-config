e29703471a19fc9001e57bc19b231ad60d524180
Kiyotoko <karl.zschiebsch@gmail.com>
2025-04-25 10:45:15 +0200

    Add zig, cpp, xml and prettier extensions to vscodium

--- home-manager/vscodium.nix
+++ home-manager/vscodium.nix
@@ -10,6 +10,10 @@
       sumneko.lua
       redhat.java
       rebornix.ruby
+      tiehuis.zig
+      ms-vscode.cpptools
+      dotjoshjohnson.xml
+      esbenp.prettier-vscode
       jgclark.vscode-todo-highlight
       mechatroner.rainbow-csv
       bbenoist.nix
