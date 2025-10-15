b01d3654ee8ce05282f1e91221ca3fae3df4fd11
Kiyotoko <karl.zschiebsch@gmail.com>
2025-10-15 16:01:53 +0200

    feat(nixos): remove unused packages

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -39,18 +39,14 @@
     ashell
     hypridle
     swww
-    swaylock
     hyprshot
     hyprpicker
-    # Wayland based logout menu
-    wlogout
     # Transparency for wlogout
     gtk-layer-shell
     # Lightweight and customizable notification daemon
     dunst
     # This program allows you read and control device brightness
     brightnessctl
-    wofi
     nemo-with-extensions
     nemo-emblems
     nemo-fileroller
