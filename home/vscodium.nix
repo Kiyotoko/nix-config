63b4b24e7ace34822e3f155fe478aba40c9e07e0
Kiyotoko <karl.zschiebsch@gmail.com>
2025-11-03 11:42:41 +0100

    feat(home): add user settings

--- home/vscodium.nix
+++ home/vscodium.nix
@@ -3,24 +3,35 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    profiles.default.extensions =
-      with pkgs.vscode-extensions;
-      [
-        yzhang.markdown-all-in-one
-        golang.go
-        sumneko.lua
-        llvm-vs-code-extensions.vscode-clangd
-        dotjoshjohnson.xml
-        esbenp.prettier-vscode
-        jgclark.vscode-todo-highlight
-        mechatroner.rainbow-csv
-        bbenoist.nix
-        bradlc.vscode-tailwindcss
-        ziglang.vscode-zig
-        rust-lang.rust-analyzer
-      ]
-      ++ [
-        pkgs-unstable.vscode-extensions.oracle.oracle-java
-      ];
+    profiles.default = {
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          yzhang.markdown-all-in-one
+          golang.go
+          sumneko.lua
+          llvm-vs-code-extensions.vscode-clangd
+          dotjoshjohnson.xml
+          esbenp.prettier-vscode
+          jgclark.vscode-todo-highlight
+          mechatroner.rainbow-csv
+          bbenoist.nix
+          bradlc.vscode-tailwindcss
+          ziglang.vscode-zig
+          rust-lang.rust-analyzer
+        ]
+        ++ [
+          pkgs-unstable.vscode-extensions.oracle.oracle-java
+        ];
+
+      userSettings = {
+        "explorer.confirmDelete" = false;
+        "editor.wordWrap" = "on";
+        "editor.defaultFormatter" = "esbenp.prettier-vscode";
+        "editor.tabSize" = 2;
+        "editor.autoIndentOnPaste" = true;
+      };
+    };
   };
+
 }
