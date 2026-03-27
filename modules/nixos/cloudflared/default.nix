9fb264a4f8987fd463c36907e8d49d3c03d1ffce
Kiyotoko <karl.zschiebsch@gmail.com>
2026-03-27 22:22:22 +0100

    fix: use http and expplicit port for cloudflared routes

--- modules/nixos/cloudflared/default.nix
+++ modules/nixos/cloudflared/default.nix
@@ -6,10 +6,10 @@
       "b8e26b4e-8f71-4751-9320-f8cf76543f8b" = {
         credentialsFile = "/home/admin/.cloudflared/b8e26b4e-8f71-4751-9320-f8cf76543f8b.json";
         ingress = {
-          "zschiebsch.org" = "https://localhost";
-          "www.zschiebsch.org" = "https://localhost";
-          "git.zschiebsch.org" = "https://localhost:3000";
-          "nextcloud.zschiebsch.org" = "https://localhost:8080";
+          "zschiebsch.org" = "http://localhost:80";
+          "www.zschiebsch.org" = "http://localhost:80";
+          "git.zschiebsch.org" = "http://localhost:3000";
+          "nextcloud.zschiebsch.org" = "http://localhost:8080";
         };
         default = "http_status:404";
       };
