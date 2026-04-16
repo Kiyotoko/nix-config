69708ec7b7fd55212f6a5ca4bbf09e3e57ea2047
Kiyotoko <karl.zschiebsch@gmail.com>
2026-04-16 20:25:51 +0200

    fix: invalid address in cloudflared

--- modules/nixos/cloudflared/default.nix
+++ modules/nixos/cloudflared/default.nix
@@ -6,11 +6,9 @@
       "b8e26b4e-8f71-4751-9320-f8cf76543f8b" = {
         credentialsFile = "/home/admin/.cloudflared/b8e26b4e-8f71-4751-9320-f8cf76543f8b.json";
         ingress = {
-          "ssh.zschiebsch.org" = "http://localhost:22";
+          "ssh.zschiebsch.org" = "ssh://localhost:22";
           "zschiebsch.org" = "http://localhost:80";
           "www.zschiebsch.org" = "http://localhost:80";
-          "karl.zschiebsch.org" = "http://localhost:80/karl";
-          "willi.zschiebsch.org" = "http://localhost:80/willi";
           "git.zschiebsch.org" = "http://localhost:3000";
           "nextcloud.zschiebsch.org" = "http://localhost:8080";
         };
