e1af5045f01892ff94bd66a3e6488695ac303775
karlz <karl.zschiebsch@posteo.com>
2026-05-08 16:40:27 +0200

    feat: add bash option

--- modules/home/bash/default.nix
+++ modules/home/bash/default.nix
@@ -1,10 +1,27 @@
 {
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
+  config,
+  lib,
+  lib-flake,
+  ...
+}:
+let
+  cfg = config.kiyo.bash;
+in
+{
+  options.kiyo.bash = {
+    enable = lib-flake.kiyo.mkEnableDefault "Bash" true;
+  };
 
-    shellAliases = {
-      conf = "make -sC ~/Documents/Kiyotoko/nix-config/";
+  config = lib.mkIf cfg.enable {
+    programs.bash = {
+      enable = true;
+      enableCompletion = true;
+
+      shellAliases = {
+        get = "curl";
+        post = "curl -d";
+        typ = "typst watch --root .";
+      };
     };
   };
 }
